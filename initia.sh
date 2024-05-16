c64b021d196e5e2d7f738b72884a5f92768f2d14
dewiniwidya <119053785+dwiasw@users.noreply.github.com>
2024-05-16 08:44:41 +0700

    Update initia.sh

--- initia.sh
+++ initia.sh
@@ -53,7 +53,7 @@ cd $HOME
 rm -rf initia
 git clone https://github.com/initia-labs/initia.git
 cd initia
-git checkout v0.2.13
+git checkout v0.2.14
 make build
 sudo mv build/initiad /usr/local/bin/
 source .profile
